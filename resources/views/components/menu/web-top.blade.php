b733145b99fd8697105679efe9b6de87a5d862a7
sundar <aaranoffice@gmail.com>
2023-09-30 19:33:46 +0530

    #7 database design

--- resources/views/components/menu/web-top.blade.php
+++ resources/views/components/menu/web-top.blade.php
@@ -1,11 +1,11 @@
 <nav class="px-2 sm:px-4 py-2.5 bg-white sticky inset-0 z-10 border-b-2 border-gray-200">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a href="{{route('home')}}" class="flex items-center">
-            <div class="bg-white p-1 rounded">
-                <x-aaranUi::assets.logo.aaran class="h-7 w-auto block"/>
+            <div class="bg-white p-1 rounded py-2">
+                <x-aaranUi::assets.logo.cxlogo :icon="'light'"  class="h-7 w-auto block"/>
             </div>
             <span
-                class="self-center text-2xl font-semibold whitespace-nowrap px-2 tracking-wider">Aaran Associates</span>
+                class="self-center text-2xl font-semibold whitespace-nowrap px-2 tracking-wider">Codexsun</span>
         </a>
 
         <div class="flex items-center">
