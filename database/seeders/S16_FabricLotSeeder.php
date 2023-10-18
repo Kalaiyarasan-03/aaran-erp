50b87f94e95d63735a12e4ed397939ef1d0e769e
SUNDAR <aaransoftware@gmail.com>
2023-10-18 08:54:08 +0530

    #29 rework on fabric lot

--- database/seeders/S16_FabricLotSeeder.php
+++ database/seeders/S16_FabricLotSeeder.php
@@ -2,32 +2,31 @@
 
 namespace Database\Seeders;
 
-use App\Models\Erp\Production\FabricLot;
 use Illuminate\Database\Seeder;
 
 class S16_FabricLotSeeder extends Seeder
 {
     public static function run(): void
     {
-        FabricLot::create([
-            'vname' => 'Lot- 1',
-            'desc' => 'Lot - 1 desc',
-            'active_id' => '1',
-            'user_id' => '1'
-        ]);
-
-        FabricLot::create([
-            'vname' => 'Lot- 2',
-            'desc' => 'Lot -2 desc',
-            'active_id' => '1',
-            'user_id' => '1'
-        ]);
-
-        FabricLot::create([
-            'vname' => 'Lot- 3',
-            'desc' => 'Lot - 3 desc',
-            'active_id' => '1',
-            'user_id' => '1'
-        ]);
+//        FabricLot::create([
+//            'vname' => 'Lot- 1',
+//            'desc' => 'Lot - 1 desc',
+//            'active_id' => '1',
+//            'user_id' => '1'
+//        ]);
+//
+//        FabricLot::create([
+//            'vname' => 'Lot- 2',
+//            'desc' => 'Lot -2 desc',
+//            'active_id' => '1',
+//            'user_id' => '1'
+//        ]);
+//
+//        FabricLot::create([
+//            'vname' => 'Lot- 3',
+//            'desc' => 'Lot - 3 desc',
+//            'active_id' => '1',
+//            'user_id' => '1'
+//        ]);
     }
 }
